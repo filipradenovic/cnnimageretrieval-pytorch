975a835b626903a7786c9905da9422f05db4feca
David Miguel Susano Pinto <carandraug+dev@gmail.com>
2022-05-04 16:01:47 +0100

    cirtorch/utils/download*.py: update links to download datasets from VGG site.

--- cirtorch/utils/download.py
+++ cirtorch/utils/download.py
@@ -28,16 +28,16 @@ def download_test(data_dir):
 
         if dataset == 'oxford5k':
             src_dir = 'http://www.robots.ox.ac.uk/~vgg/data/oxbuildings'
-            dl_files = ['oxbuild_images.tgz']
+            dl_files = ['oxbuild_images-v1.tgz']
         elif dataset == 'paris6k':
             src_dir = 'http://www.robots.ox.ac.uk/~vgg/data/parisbuildings'
-            dl_files = ['paris_1.tgz', 'paris_2.tgz']
+            dl_files = ['paris_1-v1.tgz', 'paris_2-v1.tgz']
         elif dataset == 'roxford5k':
             src_dir = 'http://www.robots.ox.ac.uk/~vgg/data/oxbuildings'
-            dl_files = ['oxbuild_images.tgz']
+            dl_files = ['oxbuild_images-v1.tgz']
         elif dataset == 'rparis6k':
             src_dir = 'http://www.robots.ox.ac.uk/~vgg/data/parisbuildings'
-            dl_files = ['paris_1.tgz', 'paris_2.tgz']
+            dl_files = ['paris_1-v1.tgz', 'paris_2-v1.tgz']
         else:
             raise ValueError('Unknown dataset: {}!'.format(dataset))
 
